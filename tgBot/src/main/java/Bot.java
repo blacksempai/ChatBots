ca188e695b78f88e9b1a5917df8283adc7729916
Andriy Moskalenko <benjaminfranclin1998@gmail.com>
2020-04-29 02:49:35 +0300

    Initial Commit

--- tgBot/src/main/java/Bot.java
+++ tgBot/src/main/java/Bot.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-
 import java.util.HashMap;
 
 public class Bot extends TelegramLongPollingBot {
