4251aaf467e44be0fb3d0fcd6b33501158ddba17
Andriy Moskalenko <benjaminfranclin1998@gmail.com>
2020-04-29 03:29:26 +0300

    Initial Commit

--- tgBot/src/main/java/myapp/Bot.java
+++ tgBot/src/main/java/myapp/Bot.java
@@ -1,3 +1,5 @@
+package myapp;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -6,6 +8,7 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 import java.util.HashMap;
 
 public class Bot extends TelegramLongPollingBot {
@@ -14,16 +17,6 @@ public class Bot extends TelegramLongPollingBot {
     private static final String USERNAME = "AndryushaReshetilovskiyBot";
     private HashMap<Long, Integer> stageOfResumeByChatId = new HashMap<>();
     private HashMap<Long, String> resumeByChatId = new HashMap<>();
-
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
-        TelegramBotsApi bot = new TelegramBotsApi();
-        try {
-            bot.registerBot(new Bot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
-    }
 
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
---
+++ tgBot/src/main/java/myapp/DemoServlet.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package myapp;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+
+  @Override
+  public void init() throws ServletException {
+    ApiContextInitializer.init();
+    TelegramBotsApi bot = new TelegramBotsApi();
+    try {
+      bot.registerBot(new Bot());
+    } catch (TelegramApiRequestException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("<h1>Veniamin WebSite</h1>");
+  }
+}
